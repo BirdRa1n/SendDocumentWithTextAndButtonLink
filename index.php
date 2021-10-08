c2ba2debcb1bedffc262fba3aeef4a7f91ca6cdd
TheDarioJr <53487868+TheDarioJr@users.noreply.github.com>
2021-10-08 17:02:43 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+//$msg = strip_tags(nl2br($_POST['texto']));
+        $title = $_POST['title'];
+        $date = $_POST['delivery_date'];
+        $materia = $_POST['materia'];
+        $credts = $_POST['credts'];
+        $link = $_POST['link'];
+        $name_file = 'file.pdf';
+
+        $msg = "Olá, Você está testando o meu código\nCréditos: TheDarioJr";
+        //Mensagem que será exibida no chat
+
+        $token = '<YourToken>';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: multipart/form-data']);
+        curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot{$token}/sendDocument");
+        curl_setopt($ch, CURLOPT_ENCODING, '');
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+
+        $postFields = array(
+            'chat_id' => 'chat_id',
+            'document' => 'https://url_file' . "$name_file",
+                          //url do arquivo
+            'caption' => $msg,
+            'reply_markup' => '{"inline_keyboard":[[{"text":"Button Link","url":"' . $link . '"}]]}'
+                                //butão e link
+        );
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+        if (!curl_exec($ch))
+            echo curl_error($ch);
+        curl_close($ch);
+?>
